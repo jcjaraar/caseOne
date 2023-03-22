ebb773fd204763ad0c6a56e1b79af79e27fb9cdd
juliojaraar <juliojaraar@gmail.com>
2023-03-22 12:59:07 -0300

    Primera funcionada

---
+++ src/main/java/org/jaranas/caseOne/CaseOneApplication.java
@@ -0,0 +1,13 @@
+package org.jaranas.caseOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jaranas/caseOne/CaseOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaranas.caseOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
